07e6865508b7ba06f43dc21bc53dae1cc905334d
lyakhov83 <a.lyakhov@mail.ru>
2019-03-30 08:55:00 +0300

    Решены оба задания

--- 1.py
+++ 1.py
@@ -1,10 +1,47 @@
-# 1.В диапазоне натуральных чисел от 2 до 99 определить,
-# сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+"""
+1. Проанализировать скорость и сложность одного любого алгоритма,
+разработанных в рамках домашнего задания первых трех уроков.
+Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
+"""
+import timeit
+import cProfile
 
-for i in range(2, 10):
-    sum = 0
-    for j in range(2, 100):
-        if j % i == 0:
-            sum += 1
-    print("В диапазоне 2-99: {} чисел кратны {}".format(sum, i))
+num = int(input("Введите число: "))
+evenNum = 0
+oddNum = 0
+
+def NumCount(num, evenNum, oddNum):
+    if num == 0:
+        return evenNum, oddNum
+    else:
+        N = num % 10
+        num = num // 10
+        if N % 2 == 0:
+            evenNum += 1
+            return NumCount(num, evenNum, oddNum)
+        else:
+            oddNum += 1
+            return NumCount(num, evenNum, oddNum)
+#print("Число {} содержит: {} - четных и {} - нечетных цифр.". format(num, quantity, nextNum))
+print("Количество четных и нечетных цифр в числе равно: ", NumCount(num, evenNum, oddNum))
+
+print(timeit.timeit("NumCount(num, evenNum, oddNum)", setup = "from __main__ import NumCount, num, evenNum, oddNum"))
+cProfile.run("NumCount(num, evenNum, oddNum)")
+
+"""
+Входные данные: 285147936123546987
+
+Выходные данные: Количество четных и нечетных цифр в числе равно:  (8, 10)
+Время поиска при стандартном количестве проходов: 4.798279499999999
+
+22 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+     19/1    0.000    0.000    0.000    0.000 1.py:14(NumCount)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
 
--- 2.py
+++ 2.py
@@ -1,24 +1,63 @@
 """
-2.	Во втором массиве сохранить индексы четных элементов первого массива.
-Например, если дан массив со значениями 8, 3, 15, 6, 4, 2, то во второй массив
-надо заполнить значениями 1, 4, 5, 6
-(или 0, 3, 4, 5 - если индексация начинается с нуля),
-т.к. именно в этих позициях первого массива стоят четные числа.
+2. Написать два алгоритма нахождения i-го по счёту простого числа.
+Без использования «Решета Эратосфена»;
+Используя алгоритм «Решето Эратосфена»
 """
 
-from random import randint
+import timeit
 
-quanNum = int(input("Введите число: "))
-lst1 = [0] * quanNum
-lst2 = [0]
-for i in range(quanNum):
-    lst1[i] = int(randint(1, quanNum))
-    if lst1[i] % 2 == 0:
-        lst2.append(i)
+nums = int(input("Введите количество чисел"))
+indexNum = int(input("Укажите индекс искомого i - го простого числа: "))
 
-print("Исходный массив элементов:", lst1)
-print("Индексы четных элементов исходного массива:", lst2)
+def SieveErat(nums, indexNum):
+    lstNums = []
+    for i in range(nums + 1):
+        lstNums.append(i)
+
+    lstSimpleNums = []
+    i = 2
+    while i <= nums:
+        if lstNums[i] != 0:
+            lstSimpleNums.append(lstNums[i])
+            for j in range(i, nums + 1, i):
+                lstNums[j] = 0
+        i += 1
+    return lstSimpleNums[indexNum - 1]
+
+    #if indexNum > len(lstSimpleNums):
+    #    return "Индекс искомого i - го простого числа задан не верно"
+    #else:
+    #    return lstSimpleNums[indexNum - 1]
+
+print(SieveErat(nums, indexNum))
+print("Время поиска i - ого по счету простого числа: Решето Эратосфена - ",
+    timeit.timeit("SieveErat(nums, indexNum)", setup = 'from __main__ import SieveErat, nums, indexNum', number = 10))
+
+# Сложность - О(n)
+# Решето Эратосфена - число 11(lstSimleNums[5] in lstSimleNums=[0...100]), время поиска           0.0008319999999990557
+# Решето Эратосфена - число 229(lstSimleNums[50] in lstSimleNums=[0...1000]), время поиска        0.0033247999999996836
+# Решето Эратосфена - число 3571(lstSimleNums[500] in lstSimleNums=[0...10000]), время поиска     0.03324190000000016
+# Решето Эратосфена - число 48611(lstSimleNums[5000] in lstSimleNums=[0...100000]), время поиска  0.3188165000000005
 
 
+def WithSieveErat(nums, indexNum):
+    lstSimpleNums=[]
+    for i in range(2, nums + 1):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            lstSimpleNums.append(i)
 
+    return lstSimpleNums[indexNum - 1]
+
+print(WithSieveErat(nums, indexNum))
+print("Время поиска i - ого по счету простого числа: без использования решета Эратосфена - ",
+    timeit.timeit("WithSieveErat(nums, indexNum)", setup = 'from __main__ import WithSieveErat, nums, indexNum', number = 10))
+
+# Сложность - О(n^2)
+# Без использования решета Эратосфена - число 11(lstSimleNums[5] in lstSimleNums=[0...100]), время поиска           0.0008847999999996858
+# Без использования решета Эратосфена - число 229(lstSimleNums[50] in lstSimleNums=[0...1000]), время поиска        0.04149300000000089
+# Без использования решета Эратосфена - число 3571(lstSimleNums[500] in lstSimleNums=[0...10000]), время поиска     3.3281860000000005
+# Без использования решета Эратосфена - число 48611(lstSimleNums[5000] in lstSimleNums=[0...100000]), время поиска  265.1359817
 
